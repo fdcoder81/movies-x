c89108a7011e7340a8e9813d3d7ffdd8c662d0bf
Fdcoder81 <carloanselmi81@gmail.com>
2019-08-07 16:47:28 +0200

    Added MovieItem and MovieDetails

--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -1,24 +1,30 @@
 import React from "react";
 
-const MovieItem = props => {
-  const movie = props.movie;
+class MovieItem extends React.Component {
+  handleClick = e => {
+    this.props.handleClick(this.props.movie);
+  };
 
-  let image_url = "https://image.tmdb.org/t/p/w500";
-  image_url += props.movie.poster_path;
+  render() {
+    const movie = this.props.movie;
 
-  console.log(movie);
+    let image_url = "https://image.tmdb.org/t/p/w500";
+    image_url += this.props.movie.poster_path;
 
-  return (
-    <div className="card my-4">
-      <img className="card-img-top" src={image_url} alt="Card cap" />
-      <div className="card-body">
-        <h5 className="card-title">{movie.title}</h5>
-        <a href="#" className="btn btn-primary">
-          More Info
-        </a>
+    console.log(movie);
+
+    return (
+      <div className="card my-4">
+        <img className="card-img-top" src={image_url} alt="Card cap" />
+        <div className="card-body">
+          <h5 className="card-title">{movie.title}</h5>
+          <a onClick={this.handleClick} href="#" className="btn btn-primary">
+            More Info
+          </a>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default MovieItem;
--- src/components/ShowMovies.js
+++ src/components/ShowMovies.js
@@ -3,22 +3,26 @@ import MovieItem from "./MovieItem";
 import MovieDetails from "./MovieDetails";
 
 class ShowMovies extends React.Component {
-  state = { showDetail: false };
+  state = { showDetails: false };
+
+  handleClick = movie => {
+    console.log(movie);
+  };
 
   render() {
     const movies = this.props.moviesData;
 
     const list = movies.map(movie => {
       return (
-        <div>
-          <MovieItem movie={movie} />
+        <div key={movie.id}>
+          <MovieItem handleClick={this.handleClick} movie={movie} />
         </div>
       );
     });
 
     return (
       <div>
-        {!this.state.showDetail ? (
+        {!this.state.showDetails ? (
           <div className="card-deck">{list}</div>
         ) : (
           <div>
