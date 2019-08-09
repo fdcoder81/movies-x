a4b079dfa03c8dc5cd27e26fffe129037efa2a8b
Fdcoder81 <carloanselmi81@gmail.com>
2019-08-09 12:27:26 +0200

    Updates

---
+++ precache-manifest.0b5dcb278d970c8a8eea10a765e12ddf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f9158ec888bc57e9edc90a9788d0d99",
+    "url": "/movies-x/index.html"
+  },
+  {
+    "revision": "9d213cde725127fb067b",
+    "url": "/movies-x/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "54d3394e1bc2bb8d6d7a",
+    "url": "/movies-x/static/css/main.3b4744d0.chunk.css"
+  },
+  {
+    "revision": "9d213cde725127fb067b",
+    "url": "/movies-x/static/js/2.9f63ef2a.chunk.js"
+  },
+  {
+    "revision": "54d3394e1bc2bb8d6d7a",
+    "url": "/movies-x/static/js/main.9e64f48e.chunk.js"
+  },
+  {
+    "revision": "d5223e9c17fd0e148a85",
+    "url": "/movies-x/static/js/runtime~main.2168790a.js"
+  },
+  {
+    "revision": "398a6fa0902f42b38c7dad067cf1e540",
+    "url": "/movies-x/static/media/no-image.398a6fa0.jpg"
+  }
+]);
